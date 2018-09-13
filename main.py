182ebda2413f68556b8da3561ac52c39c89d6954
Tran Van Minh <s3585377@rmit.edu.vn>
2018-09-13 22:36:45 +0700

    a basic API server

--- main.py
+++ main.py
@@ -6,42 +6,20 @@ from pprint import pprint
 from gensim import corpora, models, similarities
 from sklearn.datasets import fetch_20newsgroups
 
-NO_ABOVE = 0.5
-
-NO_BELOW = 5
-
-NUM_SEARCH_RESULTS_TAKE = 10
+import config
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-NUM_TOPICS = 10000
-
-DATA_PATH = 'data'
-
-DATASET_NAME = '20newsgroups'
-
-CORE_PATH = (DATA_PATH, DATASET_NAME)
-
-LSI_MODEL_PATH = '%s/%s.lsi' % CORE_PATH
-
-CORPUS_PATH = '%s/%s.mm' % CORE_PATH
-
-NEWSGROUP_DICT_PATH = '%s/%s.dict' % CORE_PATH
-
-TFID_MODEL_PATH = '%s/%s.tfid' % CORE_PATH
-
-INDEX_PATH = '%s/%s.index' % CORE_PATH
-
 
 def create_data_path_if_not_exit():
-    data_dir_path = Path(DATA_PATH)
+    data_dir_path = Path(config.DATA_PATH)
     if not data_dir_path.exists():
         data_dir_path.mkdir()
 
 
 def load_newsgroup_dict() -> corpora.dictionary:
     dictionary = corpora.Dictionary()
-    return dictionary.load(NEWSGROUP_DICT_PATH)
+    return dictionary.load(config.NEWSGROUP_DICT_PATH)
 
 
 def convert_data_to_dict():
@@ -52,7 +30,7 @@ def convert_data_to_dict():
 def preprocess_dict(dictionary):
     dictionary.filter_n_most_frequent(5)
     # fixme, when NO_BELOW or NO_ABOVE change, this will not automatically recreate dict
-    dictionary.filter_extremes(no_below=NO_BELOW, no_above=NO_ABOVE)
+    dictionary.filter_extremes(no_below=config.NO_BELOW, no_above=config.NO_ABOVE)
     return dictionary
 
 
@@ -63,7 +41,7 @@ def get_data():
 def load_dict():
     global corpora_dict
     logging.info("*** Start loading dictionary ***")
-    dict_path = Path(NEWSGROUP_DICT_PATH)
+    dict_path = Path(config.NEWSGROUP_DICT_PATH)
     if dict_path.exists():
         corpora_dict = load_newsgroup_dict()
     else:
@@ -73,41 +51,41 @@ def load_dict():
         corpora_dict = convert_data_to_dict()
         # TODOx look inside
         corpora_dict = preprocess_dict(corpora_dict)
-        corpora_dict.save(NEWSGROUP_DICT_PATH)
+        corpora_dict.save(config.NEWSGROUP_DICT_PATH)
 
 
 def load_corpus():
     global corpus
     logging.info("*** Start loading corpus ***")
-    corpus_path = Path(CORPUS_PATH)
+    corpus_path = Path(config.CORPUS_PATH)
     if corpus_path.exists():
-        corpus = corpora.MmCorpus(CORPUS_PATH)
+        corpus = corpora.MmCorpus(config.CORPUS_PATH)
     else:
         corpus = [corpora_dict.doc2bow(text) for text in texts]
-        corpora.MmCorpus.serialize(CORPUS_PATH, corpus)
+        corpora.MmCorpus.serialize(config.CORPUS_PATH, corpus)
 
 
 def load_lsi_model():
     global lsi
     logging.info("*** Start loading LSI model ***")
-    lsi_model_path = Path(LSI_MODEL_PATH)
+    lsi_model_path = Path(config.LSI_MODEL_PATH)
     if lsi_model_path.exists():
-        lsi = models.LsiModel.load(LSI_MODEL_PATH)
+        lsi = models.LsiModel.load(config.LSI_MODEL_PATH)
     else:
         lsi = models.LsiModel(corpus_tfidf, id2word=corpora_dict,
-                              num_topics=NUM_TOPICS)  # initialize an LSI transformation
-        lsi.save(LSI_MODEL_PATH)
+                              num_topics=config.NUM_TOPICS)  # initialize an LSI transformation
+        lsi.save(config.LSI_MODEL_PATH)
 
 
 def load_tfid_model():
     global tfidf
     logging.info("*** Start loading tfid model ***")
-    tfid_model_path = Path(TFID_MODEL_PATH)
+    tfid_model_path = Path(config.TFID_MODEL_PATH)
     if tfid_model_path.exists():
-        tfidf = models.TfidfModel.load(TFID_MODEL_PATH)
+        tfidf = models.TfidfModel.load(config.TFID_MODEL_PATH)
     else:
         tfidf = models.TfidfModel(corpus)
-        tfidf.save(TFID_MODEL_PATH)
+        tfidf.save(config.TFID_MODEL_PATH)
 
 
 def load_model():
@@ -136,13 +114,13 @@ def process_args():
 def load_index():
     global index
     logging.info("*** Start loading index ***")
-    index_path = Path(INDEX_PATH)
+    index_path = Path(config.INDEX_PATH)
     if index_path.exists():
-        index = similarities.MatrixSimilarity.load(INDEX_PATH)
+        index = similarities.MatrixSimilarity.load(config.INDEX_PATH)
     else:
         # fixme the case when NUM_TOPICS change
-        index = similarities.MatrixSimilarity(corpus_lsi, num_features=NUM_TOPICS)
-        index.save(INDEX_PATH)
+        index = similarities.MatrixSimilarity(corpus_lsi, num_features=config.NUM_TOPICS)
+        index.save(config.INDEX_PATH)
 
 
 def project_query_to_lsi_space():
@@ -156,7 +134,7 @@ def find_top_n_results():
     global top_results
     sims = index[vec_lsi]
     sims = sorted(enumerate(sims), key=lambda item: -item[1])
-    top_results = sims[slice(0, NUM_SEARCH_RESULTS_TAKE)]
+    top_results = sims[slice(0, config.NUM_SEARCH_RESULTS_TAKE)]
 
 
 def find_top_n_articles():
